8331e47f1b54bb6eaebfb6db20edb4d51830fbf0
Tausif282 <rahtausi@gmail.com>
2020-07-17 08:43:31 +0530

    My very first commit

--- public/js/client.js
+++ public/js/client.js
@@ -12,3 +12,22 @@ window.setTimeout(function () {
         transition: "ease all 0.5s",
     });
 }
+
+var searchInput="search_input";
+$(document).ready(function(){
+    var autocomplete;
+    autocomplete =new google.maps.places.Autocomplete((document.getElementById(searchInput)),{
+        type:['geocode']
+    });
+    google.maps.event.addListner(autocomplete,'place_changed',function(){
+         var near_place =autocomplete.getPlace();
+         document.getElementById('latitude_input').value=near_place.geometry.location.lat();
+         document.getElementById('longitude_input').value=near_place.geometry.location.lng();
+        })
+
+});
+$(document).on('change','#'+searchInput,function(){
+    document.getElementById('latitude_input').value='';
+    document.getElementById('longitude_input').value='';
+    
+})
